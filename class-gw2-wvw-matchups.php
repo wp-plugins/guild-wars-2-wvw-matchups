9f2f847ea0b82c0e250843723c0488d22acc8315
klaufel <klaufel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-22 16:27:08 +0000

    Version 1.0.1 released
    git-svn-id: https://plugins.svn.wordpress.org/guild-wars-2-wvw-matchups/trunk@730040 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ class-gw2-wvw-matchups.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Guild Wars 2 - WvW Matchups
+ *
+ * @package   guild-wars-2-wvw-matchups
+ * @author    Klaufel <klaufel@gmail.com>
+ * @license   GPL-2.0+
+ * @link      http://www.klaufel.com/guild-wars-2-wvw-matchups
+ */
+
+class class_gw2_wvw_matchups {
+	protected $version = '1.0.1';
+	protected $plugin_slug = 'gw2_wvw_matchups';
+	protected static $instance = null;
+	protected $plugin_screen_hook_suffix = null;
+
+	private function __construct() {
+		add_action( 'init', array( $this, 'load_plugin_textdomain' ) );
+		add_action( 'gw2_wvw_matchups', array( $this, 'action_method_name' ) );
+		add_filter( 'gw2_wvw_matchups', array( $this, 'filter_method_name' ) );
+	}
+	
+	public static function get_instance() {
+		if ( null == self::$instance ) { self::$instance = new self; }
+		return self::$instance;
+	}
+
+	public static function activate( $network_wide ) { }
+	public static function deactivate( $network_wide ) { }
+
+	public function load_plugin_textdomain() {
+		$domain = $this->plugin_slug;
+		$locale = apply_filters( 'plugin_locale', get_locale(), $domain );
+		load_textdomain( $domain, WP_LANG_DIR . '/' . $domain . '/' . $domain . '-' . $locale . '.mo' );
+		load_plugin_textdomain( $domain, FALSE, dirname( plugin_basename( __FILE__ ) ) . '/lang/' );
+	}
+}
---
+++ uninstall.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Guild Wars 2 - WvW Matchups
+ *
+ * @package   guild-wars-2-wvw-matchups
+ * @author    Klaufel <klaufel@gmail.com>
+ * @license   GPL-2.0+
+ * @link      http://www.klaufel.com/guild-wars-2-wvw-matchups
+ */
+
+// If uninstall, not called from WordPress, then exit
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit;
+}
+
+// guild-wars-2-wvw-matchups: Define uninstall functionality here
